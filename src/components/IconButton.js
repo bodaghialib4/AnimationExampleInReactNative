7c1f0c11fb0cfacd95adb140c67bef8269cee0b3
bodaghialib4 <bodaghialib4@gmail.com>
2019-08-24 00:14:37 +0430

    working with Rotate Animation

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,26 +1,58 @@
 import React,{Component} from "react";
-import {TouchableWithoutFeedback,View} from "react-native";
+import {
+    TouchableWithoutFeedback,
+    View,
+    Animated,
+    Easing
+} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 export default class IconButton extends Component<{}> {
 
-    render() {
-        const { icon, onPress, data } = this.props;
+    constructor(props) {
+        super(props)
+        this.rotateValue = new Animated.Value(0); //declare animated value
+    }
 
-        return(
+    render() {
+        const {icon, onPress, data} = this.props;
+
+
+        let rotation = this.rotateValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: ["0deg", "360deg"] // degree of rotation
+        });
+
+        let transformStyle = {transform: [{rotate: rotation}]};
+
+        return (
             <TouchableWithoutFeedback
-                onPress={()=>{
+                onPress={() => {
                     onPress(data);
                 }}
+                onPressIn={() => {
+                    Animated.timing(this.rotateValue, {
+                        toValue: 1,
+                        duration: 750,
+                        easing: Easing.linear
+                    }).start()
+                }}
+                onPressOut={() => {
+                    Animated.timing(this.rotateValue, {
+                        toValue: 0,
+                        duration: 500,
+                        easing: Easing.linear
+                    }).start()
+                }}
             >
-                <View>
+                <Animated.View style={transformStyle}>
                     <Icon
                         name={icon}
                         style={styles.icon}
                         size={15}
                         color={"#586069"}
                     />
-                </View>
+                </Animated.View>
 
             </TouchableWithoutFeedback>
         );
