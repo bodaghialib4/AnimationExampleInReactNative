76f3d827dccfd3313164e5138d2c44962fdf4b0a
bodaghialib4 <bodaghialib4@gmail.com>
2019-08-30 17:45:26 +0430

    working with Spring Animation

--- src/components/AnimatedModal.js
+++ src/components/AnimatedModal.js
@@ -1,6 +1,13 @@
 import React, {Component} from "react";
 import Header from "./Header";
-import {Dimensions, TouchableOpacity, View, Text} from "react-native";
+import {
+    Dimensions,
+    TouchableOpacity,
+    View,
+    Text,
+    Animated,
+    Easing
+} from "react-native";
 let { width, height } = Dimensions.get('window');
 if (width > height) {
     let t = height;
@@ -9,20 +16,49 @@ if (width > height) {
 }
 
 export default class AnimatedModal extends Component<{}>{
+    constructor(props){
+        super(props);
+        this.yTranslate = new Animated.Value(0)
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.visible) {
+            // animate the showing of the modal
+            this.yTranslate.setValue(0); //// reset the animated value
+            Animated.spring(this.yTranslate, {
+                toValue: 1,
+                friction: 6,// how much friction to apply to the spring
+            }).start();
+        } else {
+            // animate the hiding of the modal
+            Animated.timing(this.yTranslate, {
+                toValue: 0,
+                duration: 200,
+                easing: Easing.linear,
+            }).start();
+        }
+    }
+
     render(){
         const {title, image,children, onClose} = this.props;
 
-        let bottomStyle = this.props.visible? {bottom:0}:{bottom:-height};
+        let negativeHeight = -height + 20;
+        let modalMoveY = this.yTranslate.interpolate({
+            inputRange:[0,1],
+            outputRange: [0,negativeHeight]
+        });
+
+        let translateStyle = {transform: [{translateY: modalMoveY}]};
 
         return(
-            <View style={[styles.container, bottomStyle]}>
+            <Animated.View style={[styles.container, translateStyle]}>
                 <Header title={title}>
                     <TouchableOpacity onPress={onClose}>
                         <Text style={styles.closeText}>Close</Text>
                     </TouchableOpacity>
                 </Header>
                 <View style={styles.modalContent}>{children}</View>
-            </View>
+            </Animated.View>
         );
     }
 }
