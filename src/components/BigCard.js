c012472994d21f4a5fe908a591e36cc4af6e1733
bodaghialib4 <bodaghialib4@gmail.com>
2019-08-31 23:15:44 +0430

    working with Sequence Animation

--- src/components/BigCard.js
+++ src/components/BigCard.js
@@ -1,5 +1,12 @@
 import React , {Component} from 'react';
-import {Dimensions, View, Image, Text} from  'react-native';
+import {
+    Dimensions,
+    View,
+    Image,
+    Text,
+    Animated,
+    Easing
+} from  'react-native';
 import DataRow from "./DataRow"
 let {width,height} = Dimensions.get('window');
 if( width > height ) {
@@ -10,21 +17,84 @@ if( width > height ) {
 
 
 export default class BigCard extends Component<{}> {
+    constructor(props){
+        super(props);
+        this.imageOpacityValue = new Animated.Value(0);
+        this.titleTranslateYValue = new Animated.Value(0);
+        this.titleScaleValue = new Animated.Value(0);
+    }
+
+    componentDidUpdate() {
+        //reset the animation values
+        this.imageOpacityValue.setValue(0);
+        this.titleTranslateYValue.setValue(0);
+        this.titleScaleValue.setValue(0);
+
+        //start the sequence
+        Animated.sequence([
+            Animated.timing(this.imageOpacityValue, {
+                toValue: 1,
+                duration: 1000,
+                easing: Easing.linear
+            }),
+            Animated.timing(this.titleTranslateYValue, {
+                toValue: 1,
+                duration: 400,
+                easing: Easing.linear
+            }),
+            Animated.timing(this.titleScaleValue, {
+                toValue: 1,
+                duration: 300,
+                easing: Easing.linear
+            })
+        ]).start();
+    }
+
     render() {
         const {title,price, image, data} = this.props;
+        //interpolate the image opacity
+        const imageOpacity = this.imageOpacityValue.interpolate({
+            inputRange: [0, 0.25, 0.5, 0.75, 1],
+            outputRange: [0, 0.25, 0.5, 0.75, 1]
+        });
+        //construct the image style
+        const imageOpacityStyle={
+            opacity: imageOpacity
+        };
+
+        //interpolate the vertial position of the title
+        const titleMoveY = this.titleTranslateYValue.interpolate({
+            inputRange:[0,1],
+            outputRange:[0,height/2]
+        });
+        //interpolate the scale of the title
+        const titleScale = this.titleScaleValue.interpolate({
+            inputRange:[0,0.5,1],
+            outputRange:[0.25,0.5,1]
+        });
+        //construct the style for the title
+        const titleTransformStyle={
+            transform:[{translateY: titleMoveY},{scale: titleScale}]
+        };
+
+
+
 
         return (
             <View style={styles.container}>
                 <View style={styles.mainContainer}>
-                    <Image source={image} style={styles.image} resizeMode={"contain"}/>
-                    <View style={styles.titleContainer}>
+                    <Animated.Image
+                        source={image}
+                        style={[styles.image,imageOpacityStyle]}
+                        resizeMode={"contain"}/>
+                    <Animated.View style={[styles.titleContainer,titleTransformStyle]}>
                         <Text style={styles.title}>
                             {title}
                         </Text>
                         <Text style={styles.price}>
                             {"price: " + price + "$"}
                         </Text>
-                    </View>
+                    </Animated.View>
                 </View>
                 {data && (
                     <View style={styles.dataContainer}>
@@ -54,10 +124,21 @@ const styles = {
     container: {
         flex: 1,
     },
+    mainContainer: {
+        minHeight: height/2.5,
+        alignItems: "center",
+    },
+    image: {
+        width: width/1.1,
+        minHeight: height/4,
+    },
     titleContainer: {
         height:height/13,
         alignItems:'center',
-        justifyContent:'space-around'
+        justifyContent:'space-around',
+        position: 'absolute',
+        top:-height/5.2,
+
     },
     title: {
         fontSize: 25,
@@ -65,15 +146,6 @@ const styles = {
     },
     price: {
         fontSize: 25,
-    },
-    mainContainer: {
-        minHeight: height/2.8,
-        justifyContent: "space-around",
-        alignItems: "center"
-    },
-    image: {
-        width: width/1.1,
-        minHeight: height/4,
     },
     dataContainer: {
         minHeight: height/3,
